1689c065575773021ecc99e961f3c1c305397622
DoanSy <99658269+DoanSy16@users.noreply.github.com>
2023-09-23 15:21:03 +0700

    five commit

--- src/main/java/com/davisy/entity/Interested.java
+++ src/main/java/com/davisy/entity/Interested.java
@@ -1,5 +1,7 @@
 package com.davisy.entity;
 
+import java.util.Date;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -7,6 +9,8 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,13 +24,16 @@ public class Interested {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	int interested_id;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	User user;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "post_id")
 	Post post;
+	
+	@Temporal(TemporalType.DATE)
+	Date date_interested = new Date();
 
 }
